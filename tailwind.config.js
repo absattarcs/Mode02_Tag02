57152732efa60a4f77ce07774cf499ae180bbb45
absattarcs <absattar.cs@gmail.com>
2024-12-19 23:57:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-color': '#505393', // Replace with your desired color
+        'another-color': '#D3D4E4', // Another custom color
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
